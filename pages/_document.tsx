9a8fe5e51ddd15a4f7bb6971e53a88cf00e553e8
Cody Brunner <rockchalkwushock@icloud.com>
2021-04-22 11:47:49 -0500

    feat(root): adds dev-tooling and config, adds ci/cd workflows, adds initial app

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-US" prefix="og: http://ogp.me/ns#">
+        <Head />
+        <body className="bg-primary text-primary">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">
+        Hello NextJS + TailwindCSS + TypeScript Starter
+      </h1>
+    </div>
+  )
+}
+
+export default Home
